edc1fb3c9746c9eb2aaea76ebcf3237141341c5b
sameerali306 <sameeraly240@gmail.com>
2025-02-26 23:38:46 +0500

    first commit

---
+++ claas40.py
@@ -0,0 +1,4 @@
+f1=open("cars.txt","r")
+# f1.write("hello")
+
+print(f1.read())
---
+++ class26.py
@@ -0,0 +1,19 @@
+# i=1
+# while i<=5:
+#     print(i)
+#     if(i==3):
+#         break
+#     i +=1
+# print("Your loop is break temperiory")    
+
+
+nums = [1, 23, 43, 56, 98, 1, 5, 78, 23, 54]
+x = 5
+i = 0
+while i < len(nums):
+    if nums[i] == x:
+        print("FOUND at index", i)
+        break
+    else:
+        print("finding")
+    i += 1  # Increment the index to avoid an infinite loop
