d344b1e31d56bd954d3c2588cd693cf5ef354085
sameerali306 <sameeraly240@gmail.com>
2025-03-03 14:59:03 +0500

    main

---
+++ folder43/class47.py
@@ -0,0 +1,11 @@
+# Small Python Program
+
+# Ask for user's name and age
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+
+# Calculate age in 5 years
+age_in_5_years = age + 5
+
+# Print a message
+print(f"Hello {name}! In 5 years, you will be {age_in_5_years} years old.")
