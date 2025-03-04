25a064f38b73c559cd1c926e2ebb275bcf343347
sameerali306 <sameeraly240@gmail.com>
2025-03-04 13:08:23 +0500

    lood

---
+++ folder43/if statment/index.py
@@ -0,0 +1,48 @@
+a=10
+b=20
+b>a
+print("b is greater than a")
+
+a=10
+b=1
+if a==b:
+    print("a and b are equal")
+elif a!=b:
+   print(" a and b are not equal")
+
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+a=30
+b=20
+if a>b:
+   print("a is greater than b")
+elif a <b:
+   print("b is greater than a")
+elif a==b:
+   print("a and b are equal")
+
+a=545
+b=56
+c=50
+if a>b>c:
+    print("a is greater of all")
+elif a<b>c:
+    print("b is greater than all other")
+elif a<b<c:
+    print("c is greater of all time")
+elif a==b<c:
+    print("a and b are equal but c is greater than a and b")
+elif a>b==c:
+    print("b and c are equal and a is greater")
+elif a==c<b:
+    print("a and c are equal and b is greater")
+elif a>b<c:
+    print("b is smaller of all")
+elif a<b<c:
+    print("a is smaller of all")
+else:
+    print("the given condition is beyod my limit")    
