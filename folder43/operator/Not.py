2c87897074d54f5f3e1d2c3c87da2c0477614f04
sameerali306 <sameeraly240@gmail.com>
2025-03-05 15:34:57 +0500

    main

--- folder43/operator/Not.py
+++ folder43/operator/Not.py
@@ -1,9 +1,9 @@
-# a=300
-# b=40
-# if not a>b:
-#     print("the condition is true")
-# else:
-#     print("the above condition is not true")    
+a=300
+b=40
+if not a>b:
+    print("the condition is true")
+else:
+    print("the above condition is not true")    
 
 a=300
 b=200
