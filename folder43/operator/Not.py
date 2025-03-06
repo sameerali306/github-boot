743c2b45a691a675a82d1e53a7ac4db0a3fa3900
sameerali306 <sameeraly240@gmail.com>
2025-03-06 23:14:23 +0500

    variables

--- folder43/operator/Not.py
+++ folder43/operator/Not.py
@@ -1,13 +1,32 @@
-a=300
-b=40
-if not a>b:
-    print("the condition is true")
-else:
-    print("the above condition is not true")    
+# a=300
+# b=40
+# if not a>b:
+#     print("the condition is true")
+# else:
+#     print("the above condition is not true")    
 
-a=300
-b=200
-if not a>b:
-    print("the above statment is not true")    
-else:
-    print("the above statment is true")    
+# a=300
+# b=200
+# if not a>b:
+#     print("the above statment is not true")    
+# else:
+#     print("the above statment is true")    
+
+
+# from wifipasswords import Wifipassword
+
+# # Initialize Wifipassword objects
+# passwords = Wifipassword()
+# connect_password = Wifipassword()
+
+# # Get saved wifi passwords
+# saved_passwords = passwords.get_passwords()
+
+# # Get currently connected wifi password
+# currently_connected_password = connect_password.get_currently_connect_passwords()
+
+# # Print the results
+# print(saved_passwords)
+# print(currently_connected_password)
+import wifipasswords
+print(dir(wifipasswords))
---
+++ variable/variable.py
@@ -0,0 +1,41 @@
+# variable are the cantainer use to store data
+# A variable is created in this time when asign a value
+a=5
+b="sameer"
+# print(a)
+# print(b)
+d="ali"
+# print(d)
+
+# casting of a variable mean define the type of a variable
+e=int(3)
+# print(e)
+f=str(3)
+# print(f)
+g=float(3)
+# print(g)
+
+# get the data type of variable
+# print(type(e))
+# print(type(f))
+# print(type(g))
+
+# string can be declare either single or double code
+i="john"
+i='john'
+# print(i)
+
+# case-sensative
+# variable name are caase sensatative mean that upper case letter is consider different from  lower case letter
+a="sameer"
+A="ali"
+# print(A)
+
+# variable name
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(_my_var)
