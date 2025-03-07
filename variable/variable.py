da23f8648c96bd1b1e666034ff83337f071c05cf
sameerali306 <sameeraly240@gmail.com>
2025-03-07 22:05:25 +0500

    variable

--- variable/variable.py
+++ variable/variable.py
@@ -1,41 +1,70 @@
 # variable are the cantainer use to store data
 # A variable is created in this time when asign a value
-a=5
-b="sameer"
-print(a)
-print(b)
-d="ali"
-print(d)
-
+# a=5
+# b="sameer"
+# print(a)
+# print(b)
+# d="ali"
+# print(d)
+# a="Hello....."
+# print(a)
+# b="hello my name is sameer ali and i am from gupis jandrote"
+# print(b)
 # casting of a variable mean define the type of a variable
-e=int(3)
-print(e)
-f=str(3)
-print(f)
-g=float(3)
-print(g)
+# e=int(3)
+# print(e)
+# f=str(3)
+# print(f)
+# g=float(3)
+# print(g)
+# e=int(2)
+# print(e)
+# print(type (e))
 
 # get the data type of variable
-print(type(e))
-print(type(f))
-print(type(g))
+# print(type(e))
+# print(type(f))
+# print(type(g))
 
 # string can be declare either single or double code
-i="john"
-i='john'
-print(i)
+# i="john"
+# i='john'
+# print(i)
 
 # case-sensative
 # variable name are caase sensatative mean that upper case letter is consider different from  lower case letter
-a="sameer"
-A="ali"
-print(A)
+# a="sameer"
+# A="ali"
+# print(A)
 
 # variable name
-myvar = "John"
-my_var = "John"
-_my_var = "John"
-myVar = "John"
-MYVAR = "John"
-myvar2 = "John"
-print(_my_var)
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+# print(_my_var)
+
+
+a="sameer ali"
+print(a)
+b=3
+print(b)
+# casting
+a=str("shahzain alam")
+print(a)
+print(type (a))
+b=int(5)
+print(b)
+print(type (b))
+c=float(34.44)
+print("the value of c is =",c)
+print(type (c))
+
+# case_sensative
+a="sameer"
+A="ali"
+print(a)
+print(A)
+
