24d292501f5e67c135fc9fc40eb645e4fcc57968
sameerali306 <sameeraly240@gmail.com>
2025-03-08 23:50:49 +0500

    man

--- variable/data-type/data.py
+++ variable/data-type/data.py
@@ -1,33 +1,56 @@
-x=5
-print(x)
-print(type (x))
-y="sameer"
-print(y)
-print(type (y))
-z=34.33
-print(z)
-print(type (z))
-m=4j
-print(m)
-print(type (m))
-n=["apple","mange","orange"]
-print(n)
-print(type (n))
-x={"name":"jhon","age":20}
-print(x)
-print(type (x))
-x={"mange","apple","apppricote"}
-print(x)
-print(type (x))
-z=frozenset(["apple","pineapple","orange"])
-print(z)
-print(type (z))
-s="true"
-print(s)
-print(type (s))
-g=b"hello"
-print(g)
-print(type (g))
-k=("apple","mange")
-print(k)
-print(type (k))
+# x=5
+# print(x)
+# print(type (x))
+# y="sameer"
+# print(y)
+# print(type (y))
+# z=34.33
+# print(z)
+# print(type (z))
+# m=4j
+# print(m)
+# print(type (m))
+# n=["apple","mange","orange"]
+# print(n)
+# print(type (n))
+# x={"name":"jhon","age":20}
+# print(x)
+# print(type (x))
+# x={"mange","apple","apppricote"}
+# print(x)
+# print(type (x))
+# z=frozenset(["apple","pineapple","orange"])
+# print(z)
+# print(type (z))
+# s="true"
+# print(s)
+# print(type (s))
+# g=b"hello"
+# print(g)
+# print(type (g))
+# k=("apple","mange")
+# print(k)
+# print(type (k))
+
+x="hello"
+# print(type (x))
+x=4
+# print(type (x))
+x=1+2j
+# print(type (x))
+# print(x.real)
+# print(x.imag)
+x=["hello",12,"why","how are you"]
+# print(x)
+# print(type (x))
+x=("apple","mango","banana")
+# print(x)
+# print(type (x))
+
+# range is used to generate  squece of number 
+r=range(50)
+# print(list (r))
+# print(type (r))
+x={"name":"sameer ali","age":30}
+# print(x)
+# print(type (x))
