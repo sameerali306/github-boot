532ad8b6bf5df07a445bbb6c98e62fc65c1eda0e
sameerali306 <sameeraly240@gmail.com>
2025-03-14 13:05:56 +0500

    slicing

---
+++ variable/slicing/slicing.py
@@ -0,0 +1,19 @@
+# slicing of the string mean getting a small piece from any variable
+b="helloworld"
+print(b[2:8])
+a="school"
+print(a[2:4])
+c="nice to meet you in the school"
+print(c[2:6])
+
+# slice from starting
+d="hi i am sameer ali from jandrote"
+print(d[:6])
+
+# slice from ends
+d="you are good"
+print(d[2:])
+
+# negative slicing
+d="you are good"
+print(d[-5:-1])
