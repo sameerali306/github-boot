5da26a1022394e8773852394c6b9add1db55731c
sameerali306 <sameeraly240@gmail.com>
2025-03-14 13:30:39 +0500

    split

---
+++ variable/modify-string/modify.py
@@ -0,0 +1,22 @@
+# upper case method
+a="hello i am sameer ali"
+# print(a.upper())
+b="i ma from gilgit baltistan"
+# print(b.upper())
+
+# lower case method
+a="SAmeer alli Ypouathhxbbjxbjh"
+
+# print(a.lower())
+b="SAMEER ALI YPNARE "
+# print(b.lower())
+
+# remove the white space
+a="hello i am from gilgit baltistan"
+# print(a.strip())
+
+# replace method
+a="hello"
+# print(a.replace("hell","kil"))
+b="hello"
+print(b.split(","))
