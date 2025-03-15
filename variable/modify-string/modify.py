0c701c3977e3886924caa728e6ee0b07a4479090
sameerali306 <sameeraly240@gmail.com>
2025-03-15 05:40:27 +0500

    modify

--- variable/modify-string/modify.py
+++ variable/modify-string/modify.py
@@ -1,22 +1,47 @@
 # upper case method
 a="hello i am sameer ali"
 # print(a.upper())
+ab="sameer ali"
+print(ab.upper())
 b="i ma from gilgit baltistan"
-# print(b.upper())
-
+print(b.upper())
 # lower case method
 a="SAmeer alli Ypouathhxbbjxbjh"
-
-# print(a.lower())
+ba="PAKISTAN"
+print(ba.lower())
+print(a.lower())
 b="SAMEER ALI YPNARE "
-# print(b.lower())
+print(b.lower())
 
 # remove the white space
 a="hello i am from gilgit baltistan"
-# print(a.strip())
+print(a.strip())
+text = "   Hello, World!   "
+print(text)
+clean_text = text.strip()
+print(f"'{clean_text}'")
+
 
 # replace method
 a="hello"
+text = "Hello, World!"
+print(text)
+no_spaces = text.replace(" ", "")
+print(no_spaces)
+word="hello , i am sameer ali"
+print(word)
+# " ","" this line mean remove white string with empty string
+ans=word.replace(" ","")
+print(ans)
 # print(a.replace("hell","kil"))
 b="hello"
 print(b.split(","))
+
+
+# splits method
+# the split method remove the white space and divide each word into indivical component
+words=" hello ! , i ma  sameer    ali  "
+# print(words.split())
+word=words.split()
+ans=" ".join(word)
+print(ans)
