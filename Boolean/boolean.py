8cc4f743d298f9259fc9795bb6a4ff354093bced
sameerali306 <sameeraly240@gmail.com>
2025-03-17 22:13:40 +0500

    add

---
+++ Boolean/boolean.py
@@ -0,0 +1,14 @@
+a=200
+b=100
+if a > b:
+    print("a is greater than b") 
+else:
+    print("b is greater than a")
+#bool is a built in data type use to represent boolean value 
+# that is either true ir false
+print(bool(0))  
+print(bool(1)) 
+print(bool("")) 
+print(bool("hello")) 
+print(bool("Hello"))
+print(bool(15))
