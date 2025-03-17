738cfbfbaec8e992c4581f41f9a9d5960e014b77
sameerali306 <sameeraly240@gmail.com>
2025-03-17 21:27:11 +0500

    commit

---
+++ variable/methods-string/string.py
@@ -0,0 +1,32 @@
+txt="hello"
+ans=txt.capitalize()
+# print(ans)
+txt="Sameer Ali"
+ans=txt.casefold()
+# print(ans)
+txt = "sameer ali"
+ans = txt.center(50)  # Center the string to a total width of 50
+# print(ans)
+txt = "sameer ali"
+ans = txt.center(20, '*')  # Use '*' to fill the space
+# print(ans)
+txt="ali"
+ans=txt.center(20,"8")
+# print(ans)
+sentence="lorem you are best lorem you are nice"
+ans=sentence.count("lorem")
+# print(ans)
+txt="hello hi"
+ans=txt.encode()
+# print(ans)
+# encodin method in phython is use to convert string into bytes
+# and decoding method is use to reconvert byte to string
+txt="sameer ali"
+ans=txt.encode()
+# print(ans)
+txt=b"hello word"
+ans=txt.decode()
+print(ans)
+
+
+
