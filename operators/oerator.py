0aaed79b1ff3a1d31cc2e47c57bed9ca8c30e977
sameerali306 <sameeraly240@gmail.com>
2025-03-22 21:28:07 +0500

    operatore

---
+++ operators/oerator.py
@@ -0,0 +1,19 @@
+# arthimatic operatore
+a=5
+b=10
+print(a+b)
+a=10
+b=20
+print("addition",a+b)
+print("substraction" ,a-b)
+print("multipliction",a*b)
+print("division",a/b)
+print("modulus",a%b)
+# floor division give the qutient after division
+print("floor divisioon",a//b)  
+a=11
+b=3
+print("floor division",a/b)
+a=10
+b=10
+print(a/b)
