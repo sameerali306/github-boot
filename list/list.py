5d98bf14e30bb416c4078efd92dee291b377f7de
sameerali306 <sameeraly240@gmail.com>
2025-03-23 21:49:05 +0500

    main

---
+++ list/list.py
@@ -0,0 +1,32 @@
+# list in python are built in data type use to store
+# different items
+list=["apple","mango","banana","pineapple"]
+print(list)
+# list items are ordered changable and allow duplicate
+
+# ordered
+# list in python are ordered mean when we add item it
+# add in orderwise one after anather
+list=["apple","banana"]
+print(list)
+list.append("mango")
+list.append("pineapple")
+print(list)
+
+# changable
+# list are changable mean we can add remove the item in a list
+list=["sameer ali" , "shahzain alam","ehsan mir"]
+print(list)
+list.remove("ehsan mir")
+print(list)
+
+# allow duplication
+# list in python are same value multiple time
+list=["apple","banana","pineapple"]
+print(list)
+list.append("apple")
+print(list)
+# length
+print(len(list))
+# data type
+print(type(list))
