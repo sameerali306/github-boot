a833cd5c8e8daf6613f8b08f713ba61105111670
sameerali306 <sameeraly240@gmail.com>
2025-10-01 21:41:29 +0500

    origin

---
+++ python-pratice/2.py
@@ -0,0 +1,46 @@
+#input()=a function that that take any inut from the use and return entire data as a string
+#name=input("what is your name:")
+#age=input("Entyer you age:")
+#print(f"hello {name} and your age is {age} nice to see you here.")
+#print("hello {} and your age is {} nice to see you".format(name,age))
+#print("hello %s and your age is %s nice to see you here " %(name,age))
+
+#name =input("Please enter your name :")
+#age =input("Enter your age :")
+#print(f"Hello {name} happy birthday you are {age} year old .") #f string method
+#print("hello {f_name} happy birthday you are {f_age} year old".format(f_name=name,f_age=age)) #format method
+#print("hello %s happy birthday you are %s year old" %(name,age)) #percent method
+
+#length=float(input("Enter the length of rectangle :"))
+#width=(input("Enter the width of rectangle :"))
+# area=length * width/
+#print(f"the total area of rectangle is {length * width}")
+
+#shopping card
+
+#item=input("Enter your item name ? :")
+#price=float(input("What is it price :"))
+#quantity=float(input("how much you buy ?"))
+#total=price * quantity
+#print(f"You brought {quantity} X {item} and yoy total cost is {total}")
+
+
+#madlips game
+noun=input("Enter your noun :")
+adjective=input("Enter adjective :")
+adjective2=input("enter adjective2 :")
+noun2=input("Enter your noun2 :")
+noun3=input("Enter your noun3 :")
+noun4=input("Enter your noun4 :")
+verb=input("Enter your verb :")
+verb2=input("Enter your verb2 :")
+verb3=input("Enter your verb3 :")
+
+
+
+print(f"Yesterday I visited {noun}.")
+print(f"Where I meet a {adjective} and {adjective2} girl{noun2}.")
+print(f"we are student of {noun3} and know she is student of {noun4}.")
+print(f"Our meeting at {noun} is sudden and unrealistics.")
+print(f"I am really {verb} and really very{verb2}.")
+print (f"It is such a {verb3} moment in my whole life.")
