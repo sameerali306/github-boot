b82b5e3d844e813cfe60da74eb3f0b029e7b043b
sameerali306 <sameeraly240@gmail.com>
2025-10-06 16:01:32 +0500

    origin

---
+++ python-pratice/conditional operator/1.py
@@ -0,0 +1,11 @@
+# conditional expression =A conditional expression is one line statment in choos a value batween two value base on condition
+
+# value_if-true if condition value_if_false
+
+# number=int(input("Enter a number :"))
+# reslut="the number is greater than 50" if number>50 else "number is less than 50"
+# print(reslut)
+
+marks=int(input("Enter your marks :"))
+result2="You are pass" if marks>30 else "you are fail"
+print(result2)
