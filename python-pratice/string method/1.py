8a5b4a767281bb30d5d682747ad63041ea36d44d
sameerali306 <sameeraly240@gmail.com>
2025-10-07 12:00:41 +0500

    origin

---
+++ python-pratice/string method/1.py
@@ -0,0 +1,38 @@
+# string method
+# name=input("Enter your name :")
+# phone=input("Enter your phone #:")
+
+# result=len(name) # find the lenght of string
+# result2=name.rfind("a") #it return the first accurance of r in the string and rfind
+#                          # return last accurance
+# result3=name.capitalize()  #it capitilize the fist alphebet of word
+# result4=name.upper()    #it cspitilize all letter in strin                    
+# print(result4)
+
+
+# fname="sameer ali"
+# # one=phone.count("2") #it give a character how many time repeat in string
+# two=fname.replace("sameer ali","faria hussain")  #this method replace a give word or string to give word or string
+# # print(two)
+
+
+# userName=input("enter the user name :")
+
+# if len(userName) >12:
+#     print("Username must not greater than 12....")
+# elif not userName.find(" ")==-1:
+#     print("Your user name must not contain space....")  
+# elif  not userName.isalpha():
+#     print("Your userName must not contain digit....")     
+# else:
+#     print(f"wellcome {userName}") 
+
+
+# indexing=access element by squence using indexing()
+
+country="america"
+print(country[0]) #show thw character at index 1
+print(country[4:]) #show the characetr above 4 index
+print(country[-2]) #show characetr in reversed direction
+print(country[::2]) #this will print every second character in string
+        
